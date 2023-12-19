152a6b19bdc0f82040bdb782acf97b267afa2459
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2023-12-19 13:11:09 +0900

    UI code challenge

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
